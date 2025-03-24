156ec9e9cb86c43ae3eeee5e54119f8c19b0cc59
dragonsuperf <dragonsuperf@gmail.com>
2025-03-24 18:44:12 +0900

    update assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
+  assetPrefix: isProduction ? "https://dragonsuperf.github.io/" : "/",
 };
 
 export default nextConfig;
